592d9641448133cb168fc3cfd60ca77988e0b0e3
tdelev <tomce.delev@gmail.com>
2013-09-30 01:08:32 +0200

    Fixed problem 4 source code

--- latex/src/av1/p4.c
+++ latex/src/av1/p4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
     int a = 3, b = 5, c = 12;
-    float am = a + b + c / 3.0;
-    printf("Arithmetic mean is: %.2f\n", as);
+    float am = (a + b + c) / 3.0;
+    printf("Arithmetic mean is: %.2f\n", am);
     return 0;
 }

41830bec94cdc89c2264ac56f580306beaac5a00
emilstankov <emil.stankov@gmail.com>
2015-12-11 18:33:07 +0100

    Update p4.c

--- latex/src/av9/p4.c
+++ latex/src/av9/p4.c
@@ -7,7 +7,7 @@ int substring(char *s1, char *s2) {
     int d2 = strlen(s2);
     if (d1 > d2)
         return 0;
-    for (i = 0; i < d2 - d1; i++)
+    for (i = 0; i <= d2 - d1; i++)
         if (strncmp(s1, s2 + i, d1) == 0)
             return 1;
     return 0;

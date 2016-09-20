19391821a3dec9428e974a12d85d8663b4749357
Tomche Delev <tomce.delev@gmail.com>
2016-09-20 15:36:27 +0200

    Complete av1 and av2  - simplified build.gradle

---
+++ src/av1/ex11.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Zdravo, kako si?\n");
+    return 0;
+}
+
--- src/av1/hello.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Hello FINKI!\n");
-    return 0;
-}
-

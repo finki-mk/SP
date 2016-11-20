e11af5c0f8ea0a91ab076edae5d11fff7bb66c60
Tomche Delev <tomce.delev@gmail.com>
2016-11-20 23:24:46 +0100

    Аудиториска вежба 7 и 8

--- src/av6/p6_6_en.c
+++ src/av6/p6_6_en.c
@@ -5,15 +5,22 @@ int main() {
     int a[MAX];
     scanf("%d", &n);
 
+    int m;
+    scanf("%d", &m);
+
     for(i = 0; i < n; ++i) {
         scanf("%d", &a[i]);
     }
+    int j;
+    for(j = 0; j < m; j++) {
+		int temp = a[n-1];
+		for(i = n - 1; i > 0; i--) {
+			a[i] = a[i-1];
+		}
+		a[0] = temp;
+	}
 
-    int temp = a[n-1];
-    for(i = n - 1; i > 0; i--) {
-        a[i] = a[i-1];
-    }
-    a[0] = temp;
+    printf("\n");
 
     for(i = 0; i < n; ++i) {
         printf("%d ", a[i]);
